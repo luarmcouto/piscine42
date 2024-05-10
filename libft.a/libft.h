d6bf8996904f6b494095e6ff1f718293d6c7dfc7
Lua <luarmcouto@icloud.com>
2024-05-10 17:23:56 +0100

    alteracoes adcionadas

--- libft.a/libft.h
+++ libft.a/libft.h
@@ -6,7 +6,7 @@
 /*   By: luamonteiro <luamonteiro@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 14:56:40 by luamonteiro       #+#    #+#             */
-/*   Updated: 2024/05/10 14:56:43 by luamonteiro      ###   ########.fr       */
+/*   Updated: 2024/05/10 17:22:36 by luamonteiro      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,8 @@ void	ft_bzero(void *ptr, size_t num);
 void	*ft_memcpy(void *dest, const void *src, size_t num);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
+void	*ft_memmove(void *to, const void *from, size_t n);
+int		ft_toupper(int c);
+int		ft_toupper(int c);
 
 #endif
